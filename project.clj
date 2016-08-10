d944ca36b5004a747c8fd5e6130c4542e708a850
Abhiroop Sarkar <asiamgenius@gmail.com>
2016-08-10 11:33:27 +0530

    Add SQLite grammar for parsing

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+		 [clj-antlr "0.2.2"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler eskape.handler/app}
   :profiles
